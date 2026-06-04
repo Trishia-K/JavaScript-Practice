5b03e6d416c0db4a6a6866f70159586a39db2768
Kobumanzi Trishia <kobu2rsha@gmail.com>
2026-06-04 16:12:26 +0300

    Add fixed greeting file

--- Greeting.js
+++ Greeting.js
@@ -1,16 +1,19 @@
 
-let name = prompt("What is your name?", " ");
-let time= prompt ("Time of the day(morning, afternoon, evening", " ");
+//let name = prompt("What is your name?", " ");
+//let time= prompt ("Time of the day(morning, afternoon, evening", " ");
+
+let name= "Trishia";
+let time= "afternoon";
 
 function Greet(){
     if (time== "morning"){
-        console.log("Good morning ${name}");
+        console.log(`Good morning ${name}`);
     }
         else if (time== "afternoon"){
-        console.log("Good afternoon ${name}");
+        console.log(`Good afternoon ${name}`);
     }
         else {
-        console.log("Good evening ${name}");
+        console.log(`Good evening ${name}`);
     }
     }
 Greet();
